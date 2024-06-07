a18f8886bdebfe8760270fa92bad4c8289b73b4d
AlexC <alexander.m.cecile@gmail.com>
2024-06-07 19:37:30 -0400

    Added showcase of repos

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+List<List<String>> repos = [
+  [
+    'Assignment 1',
+    'https://github.com/AlexanderCecile/w24_app_dev_assignment_1'
+  ],
+  [
+    'Assignment 2',
+    'https://github.com/AlexanderCecile/w24_app_dev_assignment_2'
+  ],
+  ['Lab 2', 'https://github.com/AlexanderCecile/app_dev_2_lab_2'],
+  ['Final Exam', 'https://github.com/AlexanderCecile/w24_app_dev_2_exam_2']
+];
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'App Dev 2 Final Project',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Code on Github'),
       ),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Placeholder(),
-      ),
+      body: ListView.builder(
+          itemCount: repos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(repos[index][0]),
+              subtitle: InkWell(
+                child: Text(repos[index][1]),
+                onTap: () {
+                  launchUrl(Uri.parse(repos[index][1]));
+                },
+              ),
+            );
+          }),
     );
   }
 }
